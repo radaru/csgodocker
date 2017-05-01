46e6afe070fd9bccfc997748a15d13bc7af6633e
gangrena <radaru@wp.pl>
2017-05-02 01:18:53 +0200

    autoexec volume mount in example

--- docker.sh
+++ docker.sh
@@ -3,7 +3,8 @@
 
 docker run -v ~/csgo/addons:/home/steam/Steam/csgo/csgo/addons \
        -v ~/csgo/cfg/server.cfg:/home/steam/Steam/csgo/csgo/cfg/server.cfg \
+       -v ~/csgo/cfg/autoexec.cfg:/home/steam/Steam/csgo/csgo/cfg/autoexec.cfg \
        -v ~/csgo/cfg/sourcemod:/home/steam/Steam/csgo/csgo/cfg/sourcemod \
        -e GAME_TYPE=0 -e GAME_MODE=1 -e MAPGROUP=mg_active \
        -e MAP=de_dust2 -p 27015:27015 -p 27015:27015/udp \
-       -d csgo:0.21
+       -d csgo:latest
